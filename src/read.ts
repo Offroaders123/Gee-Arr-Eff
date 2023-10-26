5002559cf4e57fbfc763cd76014192ea466e4f8d
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-10-26 15:24:44 -0700

    Broken Parsing
    Trying to get the table of names parsed, but it isn't staying to the format I thought it was supposed to adhere to. This is following the 'GameRuleFileReader' implementation from the OMI library.
    
    Part of the other issue is that I'm not sure whether my decompressed version of the demo has the initial header still or not, whether it's starting with the body with the first bytes.
    
    The song of the day is "Actual Brutal Facts", been in my head as of lately. Listened to all of The Harmony Codex again, man I do like it a lot. Especially "What Life Brings".
    
    Going to go for a run now, wanted to make sure to hydrate enough before, that's been my hindrance with some of my runs. You can have enough to eat, but not having enough water is just as energy-taking.
    
    https://github.com/PhoenixARC/-OMI-Filetype-Library/blob/main/OMI%20Filetypes%20Library/Workers/GameRuleWorker/GameRuleFileReader.cs

--- src/read.ts
+++ src/read.ts
@@ -1,5 +1,20 @@
 import type { GRF } from "./format.js";
 
 export async function read(data: Uint8Array): Promise<GRF> {
+  const view = new DataView(data.buffer,data.byteOffset,data.byteLength);
+  let pointer: number = 16;
+  const decoder = new TextDecoder();
+
+  const tableSize = 2000 ?? view.getInt32((pointer+=4)-4);
+  console.log(tableSize);
+
+  const stringLookUpTable = Array<string>(tableSize);
+  for (let i = 0; i < stringLookUpTable.length; i++){
+    // ReadString
+    const stringLength = view.getInt16((pointer+=2)-2);
+    const string = decoder.decode(data.subarray(pointer,pointer+=stringLength));
+    console.log(stringLength,string);
+  }
+
   return {};
 }
